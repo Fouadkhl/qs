6c9d88c7f90bed5cd64662a3630cd5c7432c4dfe
fouadkhl <fouadkhl>
2022-03-27 13:25:49 -0400

    this is our second commit

--- app/src/main/java/com/example/githubproject/MainActivity.kt
+++ app/src/main/java/com/example/githubproject/MainActivity.kt
@@ -9,5 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         print("this is contained in my second commit ")
+
+        we  are doing something good
     }
 }
